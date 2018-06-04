590d28b0506c93eb630031d9a5c235628f8aa51c
arnav dhamija <arnav.dhamija@gmail.com>
2018-06-04 19:52:08 +0530

    Working on NTP timestamps

--- src/QoSEstimator.h
+++ src/QoSEstimator.h
@@ -12,6 +12,7 @@ class QoSEstimator {
     struct ntp_time_t {
         guint32 second;
         guint32 fraction;
+
         static ntp_time_t convert_from_unix_time(timeval unix_time)
         {
             ntp_time_t result;
@@ -19,11 +20,19 @@ class QoSEstimator {
             result.fraction = (uint32_t)((double)(unix_time.tv_usec + 1) * (double)(1LL<<32) * 1.0e-6);
             return result;
         }
+
+        static ntp_time_t get_struct_from_timestamp(guint64 full_ntp_timestamp)
+        {
+            ntp_time_t result;
+            result.second = (full_ntp_timestamp & 0xFFFF0000) >> 32;
+            result.fraction = (full_ntp_timestamp & 0x0000FFFF) >> 32;
+            return result;
+        }
     };
     static const guint64 ntp_offset = 2208988800;
 
     guint32 estimated_bitrate;
-    guint64 get_ntp_time();
+    guint64 get_current_ntp_time();
     guint32 get_compressed_ntp_time(guint64 full_ntp_timestamp);
 public:
     QoSEstimator();
