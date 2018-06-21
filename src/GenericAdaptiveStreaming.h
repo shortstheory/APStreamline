e110824859ff7d007ae3c6d7bcbd9c9f636d3459
arnav dhamija <arnav.dhamija@gmail.com>
2018-06-21 12:17:01 +0530

    Much debugging needed

--- src/GenericAdaptiveStreaming.h
+++ src/GenericAdaptiveStreaming.h
@@ -28,29 +28,19 @@ private:
 
     gint v4l2_cam_fd;
 
-    GstElement* pipeline;
-    GstElement* v4l2_src;
-    GstElement* src_capsfilter;
-    GstElement* videoconvert;
-    GstElement* h264_encoder;
-    GstElement* h264_parser;
-    GstElement* rtph264_payloader;
 
     string video_caps_string;
 
-    QoSEstimator qos_estimator;
 
     // better off as a char array, change it later
     string video_presets[3];
     guint32 bitrate_presets[3];
 
-    virtual bool link_all_elements() = 0;
 
     bool init_elements();
     void init_element_properties();
     void pipeline_add_elements();
 
-    void adapt_stream();
     void set_encoding_bitrate(guint32 bitrate);
     void set_resolution(ResolutionPresets setting);
     void increase_resolution();
@@ -60,12 +50,23 @@ private:
     void degrade_quality();
 
 public:
+    GstElement* pipeline;
+    GstElement* v4l2_src;
+    GstElement* src_capsfilter;
+    GstElement* videoconvert;
+    GstElement* h264_encoder;
+    GstElement* h264_parser;
+    GstElement* rtph264_payloader;
+
+    QoSEstimator qos_estimator;
     enum CameraType {RAW_CAM, H264_CAM};
     const CameraType camera_type;
 
     GenericAdaptiveStreaming(string _device = "/dev/video0", CameraType type = CameraType::RAW_CAM);
     ~GenericAdaptiveStreaming();
 
+    void adapt_stream();
+    virtual bool link_all_elements() = 0;
     bool play_pipeline();
     bool pause_pipeline();
     bool change_source(string _device);
