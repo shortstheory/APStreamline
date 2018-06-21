87e540a5e06e4d16d6aeaf41c83c92be7fee5168
arnav dhamija <arnav.dhamija@gmail.com>
2018-06-21 10:25:10 +0530

    Time to refactor

---
+++ src/UDPAdaptiveStreaming.h
@@ -0,0 +1,12 @@
+#ifndef UDP_ADAPTIVE_STREAMING_H
+#define UDP_ADAPTIVE_STREAMING_H
+
+#include "GenericAdaptiveStreaming.h"
+
+using namespace std;
+
+class UDPAdaptiveStreaming : public GenericAdaptiveStreaming {
+
+};
+
+#endif
