b1436ca99a520abbc9de7e134798ea4a6d960b47
arnav dhamija <arnav.dhamija@gmail.com>
2018-07-11 20:53:23 +0530

    Moved constants

--- src/Constants.h
+++ src/Constants.h
@@ -1,16 +1,18 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 #include <gst/gst.h>
+#include <vector>
 
 #ifdef __amd64__
-static const guint32 MAX_STEADY_BITRATE = 6000;
+    static const guint32 MAX_STEADY_BITRATE = 6000;
 #endif
+
 #ifdef __arm__
-static const guint32 MAX_STEADY_BITRATE = 2500;
+    static const guint32 MAX_STEADY_BITRATE = 2500;
 #endif
 
 static const guint32 MIN_STEADY_BITRATE = 500;
-static const guint32 INC_STEADY_BITRATE = 750;
+static const guint32 INC_STEADY_BITRATE = 250;
 static const guint32 DEC_STEADY_BITRATE = 1000;
 
 static const guint32 MAX_CONGESTION_BITRATE = 1000;
@@ -24,5 +26,35 @@ static const guint32 HIGH_QUAL_BITRATE = 3000;
 
 static const guint32 I_FRAME_INTERVAL = 10;
 static const guint32 SUCCESSFUL_TRANSMISSION = 5;
+
+static const vector<string> RAW_CAPS_FILTERS = {
+    "video/x-raw, width=(int)320, height=(int)240, framerate=(fraction)15/1",
+    "video/x-raw, width=(int)640, height=(int)480, framerate=(fraction)15/1",
+    "video/x-raw, width=(int)1280, height=(int)720, framerate=(fraction)15/1",
+
+    "video/x-raw, width=(int)320, height=(int)240, framerate=(fraction)30/1",
+    "video/x-raw, width=(int)640, height=(int)480, framerate=(fraction)30/1",
+    "video/x-raw, width=(int)1280, height=(int)720, framerate=(fraction)30/1",
+
+    "video/x-raw, width=(int)320, height=(int)240, framerate=(fraction)60/1",
+    "video/x-raw, width=(int)640, height=(int)480, framerate=(fraction)60/1",
+    "video/x-raw, width=(int)1280, height=(int)720, framerate=(fraction)60/1"
+};
+
+static const vector<string> H264_CAPS_FILTERS = {
+    "video/x-h264, width=(int)320, height=(int)240, framerate=(fraction)15/1",
+    "video/x-h264, width=(int)640, height=(int)480, framerate=(fraction)15/1",
+    "video/x-h264, width=(int)1280, height=(int)720, framerate=(fraction)15/1",
+
+    "video/x-h264, width=(int)320, height=(int)240, framerate=(fraction)30/1",
+    "video/x-h264, width=(int)640, height=(int)480, framerate=(fraction)30/1",
+    "video/x-h264, width=(int)1280, height=(int)720, framerate=(fraction)30/1",
+
+    "video/x-h264, width=(int)320, height=(int)240, framerate=(fraction)60/1",
+    "video/x-h264, width=(int)640, height=(int)480, framerate=(fraction)60/1",
+    "video/x-h264, width=(int)1280, height=(int)720, framerate=(fraction)60/1"
+}; 
+
+static const int AUTO_PRESET = 1024;
 
 #endif
--- src/DeviceDatatypes.h
+++ src/DeviceDatatypes.h
@@ -13,36 +13,6 @@ enum VideoPresets {VIDEO_320x240x15, VIDEO_640x480x15, VIDEO_1280x720x15,
                    VIDEO_320x240x60, VIDEO_640x480x60, VIDEO_1280x720x60
                   };
 
-const static vector<string> RAW_CAPS_FILTERS = {
-    "video/x-raw, width=(int)320, height=(int)240, framerate=(fraction)15/1",
-    "video/x-raw, width=(int)640, height=(int)480, framerate=(fraction)15/1",
-    "video/x-raw, width=(int)1280, height=(int)720, framerate=(fraction)15/1",
-
-    "video/x-raw, width=(int)320, height=(int)240, framerate=(fraction)30/1",
-    "video/x-raw, width=(int)640, height=(int)480, framerate=(fraction)30/1",
-    "video/x-raw, width=(int)1280, height=(int)720, framerate=(fraction)30/1",
-
-    "video/x-raw, width=(int)320, height=(int)240, framerate=(fraction)60/1",
-    "video/x-raw, width=(int)640, height=(int)480, framerate=(fraction)60/1",
-    "video/x-raw, width=(int)1280, height=(int)720, framerate=(fraction)60/1"
-};
-
-const static vector<string> H264_CAPS_FILTERS = {
-    "video/x-h264, width=(int)320, height=(int)240, framerate=(fraction)15/1",
-    "video/x-h264, width=(int)640, height=(int)480, framerate=(fraction)15/1",
-    "video/x-h264, width=(int)1280, height=(int)720, framerate=(fraction)15/1",
-
-    "video/x-h264, width=(int)320, height=(int)240, framerate=(fraction)30/1",
-    "video/x-h264, width=(int)640, height=(int)480, framerate=(fraction)30/1",
-    "video/x-h264, width=(int)1280, height=(int)720, framerate=(fraction)30/1",
-
-    "video/x-h264, width=(int)320, height=(int)240, framerate=(fraction)60/1",
-    "video/x-h264, width=(int)640, height=(int)480, framerate=(fraction)60/1",
-    "video/x-h264, width=(int)1280, height=(int)720, framerate=(fraction)60/1"
-}; 
-
-const static int AUTO_PRESET = 1024;
-
 enum RTSPMessageType {GET_DEVICE_PROPS, SET_DEVICE_PROPS, ERROR, COUNT};
 const static vector<string> RTSPMessageHeader = {
     "GDP", "SDP"
