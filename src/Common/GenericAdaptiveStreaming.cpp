b07503466b9b4fd077d0558c7fb4f5d311ce4333
arnav dhamija <arnav.dhamija@gmail.com>
2018-08-08 15:19:55 +0530

    Commented g_frees

--- src/Common/GenericAdaptiveStreaming.cpp
+++ src/Common/GenericAdaptiveStreaming.cpp
@@ -43,16 +43,16 @@ GenericAdaptiveStreaming::GenericAdaptiveStreaming(string _device, CameraType ty
 GenericAdaptiveStreaming::~GenericAdaptiveStreaming()
 {
     cout << "GASDtr" << sizeof(GenericAdaptiveStreaming);
-    g_free(v4l2_src);
-    g_free(src_capsfilter);
-    g_free(videoconvert);
-    g_free(h264_encoder);
-    g_free(h264_parser);
-    g_free(rtph264_payloader);
-    g_free(text_overlay);
-    g_free(tee);
-    g_free(multi_udp_sink);
-    g_free(pipeline);
+    // g_free(v4l2_src);
+    // g_free(src_capsfilter);
+    // g_free(videoconvert);
+    // g_free(h264_encoder);
+    // g_free(h264_parser);
+    // g_free(rtph264_payloader);
+    // g_free(text_overlay);
+    // g_free(tee);
+    // g_free(multi_udp_sink);
+    // g_free(pipeline);
 }
 
 void GenericAdaptiveStreaming::set_state_constants()
