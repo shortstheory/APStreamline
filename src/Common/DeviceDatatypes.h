38cf4cc33bed37a33fa835716a4646beb40e3bf3
arnav dhamija <arnav.dhamija@gmail.com>
2019-04-01 14:10:57 +0800

    Updated v4l2_info

--- src/Common/DeviceDatatypes.h
+++ src/Common/DeviceDatatypes.h
@@ -28,17 +28,6 @@ struct v4l2_info {
     string mount_point;
     CameraType camera_type;
     guint64 frame_property_bitmask;
-    // int quality;
-
-    // v4l2_info()
-    // {
-    //     quality = AUTO_PRESET;
-    // }
-
-    // void set_quality(int _quality)
-    // {
-    //     quality = _quality;
-    // }
 };
 
 // Gives QoS estimates to the Adaptive Streamer for acting upon
