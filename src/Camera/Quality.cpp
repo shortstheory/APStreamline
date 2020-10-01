85607c24d766ab62057b86427a4c7879c79825bd
arnav dhamija <arnav.dhamija@gmail.com>
2020-10-01 17:58:52 -0400

    decomposed quality class

--- src/Camera/Quality.cpp
+++ src/Camera/Quality.cpp
@@ -2,4 +2,89 @@
 #include <vector>
 using namespace std;
 
-vector<Quality> Quality::quality_table = {Quality(320, 240, 15), Quality(640, 480, 15), Quality(1280, 720, 15), Quality(320, 240, 30), Quality(640, 480, 30), Quality(1280, 720, 30), Quality(320, 240, 60), Quality(640, 480, 60), Quality(1280, 720, 60)};
+vector<Quality> Quality::quality_table = {
+        Quality(320, 240, 15), Quality(640, 480, 15), Quality(1280, 720, 15), 
+        Quality(320, 240, 30), Quality(640, 480, 30), Quality(1280, 720, 30), 
+        Quality(320, 240, 60), Quality(640, 480, 60), Quality(1280, 720, 60)
+};
+
+Quality::Quality(int quality)
+{
+    width = quality_table[quality].width;
+    height = quality_table[quality].height;
+    framerate = quality_table[quality].framerate;
+}
+
+Quality::Quality(int width_, int height_, int framerate_) : width(width_),
+    height(height_),
+    framerate(framerate_)
+{
+}
+
+// overload == operator
+int Quality::getWidth() const
+{
+    return width;
+}
+
+int Quality::getHeight() const
+{
+    return height;
+}
+
+int Quality::getFramerate() const
+{
+    return framerate;
+}
+
+Quality::QualityLevel Quality::get_quality_level()
+{
+    switch (height) {
+    case 240:
+        return QualityLevel::LOW;
+    case 480:
+        return QualityLevel::MEDIUM;
+    case 720:
+        return QualityLevel::HIGH;
+    default:
+        return QualityLevel::LOW;
+    };
+}
+
+Quality::Quality get_quality(Quality::QualityLevel quality_level)
+{
+    switch (quality_level) {
+    case Quality::QualityLevel::LOW:
+        return Quality(320, 240, 30);
+    case Quality::QualityLevel::MEDIUM:
+        return Quality(640, 480, 30);
+    case Quality::QualityLevel::HIGH:
+        return Quality(1280, 720, 30);
+    default:
+        return Quality(320, 240, 30);
+    };
+}
+
+// Hacky lookup table! Sorry folks!
+Quality Quality::int_to_Quality(int qual)
+{
+    return quality_table[qual];
+}
+
+int Quality::Quality_to_int(Quality q)
+{
+    for (unsigned long int i = 0; i < quality_table.size(); i++) {
+        if (quality_table[i] == q) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+bool Quality::operator==(Quality &rhs) const
+{
+    if (width == rhs.width && height == rhs.height && framerate == rhs.framerate) {
+        return true;
+    }
+    return false;
+}
--- src/Camera/Quality.h
+++ src/Camera/Quality.h
@@ -11,89 +11,23 @@ private:
     static vector<Quality> quality_table;
 
 public:
-    enum QualityLevel {
+    enum class QualityLevel {
         LOW,
         MEDIUM,
         HIGH
     } current_quality;
-    Quality(int quality)
-    {
-        width = quality_table[quality].width;
-        height = quality_table[quality].height;
-        framerate = quality_table[quality].framerate;
-    }
-    Quality(int width_, int height_, int framerate_) : width(width_),
-        height(height_),
-        framerate(framerate_)
-    {
-    }
+
+    Quality(int quality);
+    Quality(int width_, int height_, int framerate_);
     // overload == operator
-    int getWidth() const
-    {
-        return width;
-    }
-
-    int getHeight() const
-    {
-        return height;
-    }
-
-    int getFramerate() const
-    {
-        return framerate;
-    }
-
-    QualityLevel get_quality_level()
-    {
-        switch (height) {
-        case 240:
-            return LOW;
-        case 480:
-            return MEDIUM;
-        case 720:
-            return HIGH;
-        default:
-            return LOW;
-        };
-    }
-
-    static Quality get_quality(QualityLevel quality_level)
-    {
-        switch (quality_level) {
-        case LOW:
-            return Quality(320, 240, 30);
-        case MEDIUM:
-            return Quality(640, 480, 30);
-        case HIGH:
-            return Quality(1280, 720, 30);
-        default:
-            return Quality(320, 240, 30);
-        };
-    }
-
-    // Hacky lookup table! Sorry folks!
-    static Quality int_to_Quality(int qual)
-    {
-        return quality_table[qual];
-    }
-
-    static int Quality_to_int(Quality q)
-    {
-        for (unsigned long int i = 0; i < quality_table.size(); i++) {
-            if (quality_table[i] == q) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    bool operator==(Quality &rhs) const
-    {
-        if (width == rhs.width && height == rhs.height && framerate == rhs.framerate) {
-            return true;
-        }
-        return false;
-    }
+    int getWidth() const;
+    int getHeight() const;
+    int getFramerate() const;
+    QualityLevel get_quality_level();
+    static Quality get_quality(QualityLevel quality_level);
+    static Quality int_to_Quality(int qual);
+    static int Quality_to_int(Quality q);
+    bool operator==(Quality &rhs) const;
 };
 
 #endif
